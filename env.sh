2dbaeee6b71937c77f6a731f43166ab76a178e1b
Chris Joakim <christopher.joakim@gmail.com>
2022-02-23 13:17:55 -0500

    Added DOCKER_MONGODB_AUTH_DB to env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 # This script is 'sourced' by other scripts in this repo to define
 # common environment variables.
-# Chris Joakim, Microsoft, June 2021
+# Chris Joakim, Microsoft, February 2022
 
 # Source Database
 export DOCKER_MONGODB_URL="localhost:27017"
@@ -11,3 +11,4 @@ export DOCKER_MONGODB_HOST="localhost"
 export DOCKER_MONGODB_PORT="27017"
 export DOCKER_MONGODB_USER="root"
 export DOCKER_MONGODB_PASS="rootpassword"
+export DOCKER_MONGODB_AUTH_DB="admin"
